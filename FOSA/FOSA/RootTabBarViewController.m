2214c87a1124b338fe6120ebc05ae0cecd7c7b2e
zhijieXU <2412399060@qq.com>
2019-11-22 09:26:10 +0800

    字体

--- FOSA/FOSA/RootTabBarViewController.m
+++ FOSA/FOSA/RootTabBarViewController.m
@@ -21,10 +21,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self addChildWithVCName:@"FosaMainViewController" title:@"fosa" image:@"tabbar_logo" selectImage:@"tabbar_logoHL"];
-    [self addChildWithVCName:@"FosaProductViewController" title:@"product" image:@"tabbar_product" selectImage:@"tabbar_productHL"];
+    [self addChildWithVCName:@"FosaMainViewController" title:@"Fosa" image:@"tabbar_logo" selectImage:@"tabbar_logoHL"];
+    [self addChildWithVCName:@"FosaProductViewController" title:@"Product" image:@"tabbar_product" selectImage:@"tabbar_productHL"];
     [self addChildWithVCName:@"FosaPoundViewController" title:@"Blt" image:@"tabbar_pound" selectImage:@"tabbar_poundHL"];
-    [self addChildWithVCName:@"FosaUserViewController" title:@"me" image:@"tabbar_me" selectImage:@"tabbar_meHL"];
+    [self addChildWithVCName:@"FosaUserViewController" title:@"Me" image:@"tabbar_me" selectImage:@"tabbar_meHL"];
 }
 -(void)addChildWithVCName:(NSString *)vcName title:(NSString *)title image:(NSString *)image selectImage:(NSString *)selectImage{
     //1.创建控制器
@@ -34,6 +34,7 @@
     //2.设置控制器属性
     fosa.navigationItem.title = title;
     fosa.tabBarItem.title = title;
+    
     fosa.tabBarItem.image = [UIImage imageNamed:image];
     fosa.tabBarItem.selectedImage = [UIImage imageNamed:selectImage];
     //修改字体颜色
